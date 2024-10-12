ca669d101bac6a4f71c10820ebbe3774ddf40884
salihyusuf6 <yusuf89452@gmail.com>
2024-10-12 16:35:56 +0300

    ufak bi tık buglu

--- heceleyici.py
+++ heceleyici.py
@@ -19,19 +19,19 @@ alinan_path = "Mesaj.txt"
 
 # Dosya varsa okuma işlemi
 if Path(Time_Data_SavePath).exists():
-    with open(Time_Data_SavePath, "r", encoding="utf-8") as js_file:
+    with open(Time_Data_SavePath, "r", encoding="UTF-8") as js_file:
         Time_Data = json.load(js_file)  # Dosya içeriğini yükle
         for eklencek in Time_Data.keys():
             Time_Select += f"\n{eklencek}"
 else:
     # Dosya yoksa Base_Settings'i yaz
-    with open(Time_Data_SavePath, "w", encoding="utf-8") as js_file:
+    with open(Time_Data_SavePath, "w", encoding="UTF-8") as js_file:
         json.dump(Base_Settings, js_file, indent=4)
     Time_Data = Base_Settings  # Yalnızca yazdığımız ayarları kullan
     Time_Select = "Base"
 
 if not Path(alinan_path).exists():
-    file = open(alinan_path, "w", encoding="utf-8")
+    file = open(alinan_path, "w", encoding="UTF-8")
     file.write("base metin")
     file.close()
 
@@ -40,25 +40,16 @@ def Yazdır(mesaj_liste):
     ses_dosyasi = "mesaj.mp3"
     for mesaj in mesaj_liste:
         print(f"{i}/{len(mesaj_liste)}   :   " + mesaj)
-
-        if len(mesaj) <= 3:
-            cikti = gTTS(text=mesaj, lang="tr", slow=False)
-            cikti.save(ses_dosyasi)
-            playsound(ses_dosyasi)
-            time.sleep(tKısa)
-            os.remove(ses_dosyasi)
-        elif 4 <= len(mesaj) <= 8:
-            cikti = gTTS(text=mesaj, lang="tr", slow=False)
-            cikti.save(ses_dosyasi)
-            playsound(ses_dosyasi)
+        cikti = gTTS(text=mesaj, lang="tr", slow=False)
+        cikti.save(ses_dosyasi)
+        playsound(ses_dosyasi)
+        if len(mesaj) <= 3:            
+            time.sleep(tKısa)           
+        elif 4 <= len(mesaj) <= 8:            
             time.sleep(tNormal)
-            os.remove(ses_dosyasi)
-        elif len(mesaj) > 8:
-            cikti = gTTS(text=mesaj, lang="tr", slow=False)
-            cikti.save(ses_dosyasi)
-            playsound(ses_dosyasi)
+        elif len(mesaj) > 8:            
             time.sleep(tUzun)
-            os.remove(ses_dosyasi)
+        
             
 
         if i % molaSikligi == 0:
@@ -91,7 +82,7 @@ while checker:
         Yazdır(mesaj_splited)        
     
     elif girdi == "2":
-        with open(alinan_path, "r", encoding="utf-8") as mesaj_dosya:
+        with open(alinan_path, "r", encoding="UTF-8") as mesaj_dosya:
             mesaj = mesaj_dosya.read()
             mesaj_splited = mesaj.split(" ")
 
@@ -112,7 +103,7 @@ while checker:
         Yazdır(mesaj_splited)
 
     elif girdi == "3":
-        with open(Time_Data_SavePath, "r", encoding="utf-8") as js_file:
+        with open(Time_Data_SavePath, "r", encoding="UTF-8") as js_file:
             Time_Data = json.load(js_file)
 
         isim = input("Girdi tipi adı giriniz -->")
@@ -132,7 +123,7 @@ while checker:
 
         # Güncellenmiş veriyi yazma
         Time_Data.update(new_dictinory)
-        with open(Time_Data_SavePath, "w", encoding="utf-8") as js_file:
+        with open(Time_Data_SavePath, "w", encoding="UTF-8") as js_file:
             json.dump(Time_Data, js_file, indent=4)
             print("Girdi kaydedildi")
 
